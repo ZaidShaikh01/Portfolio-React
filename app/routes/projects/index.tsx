f480a11e93d95bf3f107ea0b528427477e1daf91
ZaidShaikh <szaid5146@gmail.com>
2026-08-07 16:37:06 +0530

    Project page started

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import { FaAngleDown, FaAngleRight } from 'react-icons/fa6';
+
 const ProjectPage = () => {
-  return <h1>Project Page</h1>;
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <div>
+      <div className='project-items'>
+        <div className='personal-accordian'>
+          <button
+            className='w-full flex items-center p-3 gap-1 text-sm text-gray-300 border-b border-b-stroke'
+            // onClick={onClick}
+          >
+            {' '}
+            {isOpen ? <FaAngleDown /> : <FaAngleRight />}
+            {'Personal'}
+          </button>
+          <div
+            className={`${isOpen ? '' : 'hidden'} border-b border-b-stroke pb-1`}
+          ></div>
+        </div>
+      </div>
+      <div className='project-container'></div>
+    </div>
+  );
 };
 
 export default ProjectPage;
