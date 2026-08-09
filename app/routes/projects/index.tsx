d8c160046b11f0697f1689e12b38cb4486af51e2
ZaidShaikh <szaid5146@gmail.com>
2026-08-09 13:36:34 +0530

    Grid colum adeed

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -3,12 +3,11 @@ import PageItems from './page-items';
 import type { Tab } from '~/types/Tabs';
 import type { ProjectData } from '~/types/ProjectData';
 import DetailsTab from '~/components/DetailsTabs';
+import GradualBlur from '~/components/GradualBlur';
 
 // Crating Id for the tabs
 
-
 const ProjectPage = () => {
-
   const technologies = [
     'React',
     'HTML',
@@ -19,37 +18,263 @@ const ProjectPage = () => {
     'Flutter',
   ];
 
-
-
   return (
     <div className='flex w-full h-full'>
       {/* Left side container */}
       <PageItems technologies={technologies} />
 
       {/* Project Container */}
-      <div className='p-10'>
+      <div className='p-10 w-full relative o'>
         {/* Details Tab will be here */}
 
         {/* Projects Details will be here */}
-        <div className="flex flex-col gap-2">
+        <div className='grid grid-cols-3 gap-6 w-full h-full '>
+          <div className='flex flex-col gap-2'>
             {/* Label Text */}
             <p>
-              <span className="text-indigo-500 mr-1">Project 1</span>
-              <span className="text-gray-400">// _ui-animations</span>
+              <span className='text-indigo-500 mr-1'>Project 1</span>
+              <span className='text-gray-400'>// _ui-animations</span>
             </p>
             {/* Actual Project Container */}
-            <div className="w-90 bg-black  rounded-2xl">
-              <div className="upper-image">
-                <img src="/images/image.png" alt="project-image" className='w-full object-cover' />
+            <div className='bg-black  rounded-2xl'>
+              <div className='upper-image'>
+                <img
+                  src='/images/image.png'
+                  alt='project-image'
+                  className='w-full object-cover'
+                />
               </div>
-               <div className="m-6 flex flex-col justify-start items-start gap-4">
-                <p className="text-gray-400 text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit, nesciunt natus velit repellat delectus, </p>
-                <button className="p-2 text-sm bg-stroke rounded-xl">
+
+              <div className='m-6 flex flex-col justify-start items-start gap-4'>
+                <p className='text-gray-400 text-sm'>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Impedit, nesciunt natus velit repellat delectus,{' '}
+                </p>
+                <button className='p-2 text-sm bg-stroke rounded-xl'>
                   View-project
                 </button>
-               </div>
+              </div>
             </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            {/* Label Text */}
+            <p>
+              <span className='text-indigo-500 mr-1'>Project 1</span>
+              <span className='text-gray-400'>// _ui-animations</span>
+            </p>
+            {/* Actual Project Container */}
+            <div className='bg-black  rounded-2xl'>
+              <div className='upper-image'>
+                <img
+                  src='/images/image.png'
+                  alt='project-image'
+                  className='w-full object-cover'
+                />
+              </div>
+
+              <div className='m-6 flex flex-col justify-start items-start gap-4'>
+                <p className='text-gray-400 text-sm'>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Impedit, nesciunt natus velit repellat delectus,{' '}
+                </p>
+                <button className='p-2 text-sm bg-stroke rounded-xl'>
+                  View-project
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            {/* Label Text */}
+            <p>
+              <span className='text-indigo-500 mr-1'>Project 1</span>
+              <span className='text-gray-400'>// _ui-animations</span>
+            </p>
+            {/* Actual Project Container */}
+            <div className='bg-black  rounded-2xl'>
+              <div className='upper-image'>
+                <img
+                  src='/images/image.png'
+                  alt='project-image'
+                  className='w-full object-cover'
+                />
+              </div>
+
+              <div className='m-6 flex flex-col justify-start items-start gap-4'>
+                <p className='text-gray-400 text-sm'>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Impedit, nesciunt natus velit repellat delectus,{' '}
+                </p>
+                <button className='p-2 text-sm bg-stroke rounded-xl'>
+                  View-project
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            {/* Label Text */}
+            <p>
+              <span className='text-indigo-500 mr-1'>Project 1</span>
+              <span className='text-gray-400'>// _ui-animations</span>
+            </p>
+            {/* Actual Project Container */}
+            <div className='bg-black  rounded-2xl'>
+              <div className='upper-image'>
+                <img
+                  src='/images/image.png'
+                  alt='project-image'
+                  className='w-full object-cover'
+                />
+              </div>
+
+              <div className='m-6 flex flex-col justify-start items-start gap-4'>
+                <p className='text-gray-400 text-sm'>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Impedit, nesciunt natus velit repellat delectus,{' '}
+                </p>
+                <button className='p-2 text-sm bg-stroke rounded-xl'>
+                  View-project
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            {/* Label Text */}
+            <p>
+              <span className='text-indigo-500 mr-1'>Project 1</span>
+              <span className='text-gray-400'>// _ui-animations</span>
+            </p>
+            {/* Actual Project Container */}
+            <div className='bg-black  rounded-2xl'>
+              <div className='upper-image'>
+                <img
+                  src='/images/image.png'
+                  alt='project-image'
+                  className='w-full object-cover'
+                />
+              </div>
+
+              <div className='m-6 flex flex-col justify-start items-start gap-4'>
+                <p className='text-gray-400 text-sm'>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Impedit, nesciunt natus velit repellat delectus,{' '}
+                </p>
+                <button className='p-2 text-sm bg-stroke rounded-xl'>
+                  View-project
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            {/* Label Text */}
+            <p>
+              <span className='text-indigo-500 mr-1'>Project 1</span>
+              <span className='text-gray-400'>// _ui-animations</span>
+            </p>
+            {/* Actual Project Container */}
+            <div className='bg-black  rounded-2xl'>
+              <div className='upper-image'>
+                <img
+                  src='/images/image.png'
+                  alt='project-image'
+                  className='w-full object-cover'
+                />
+              </div>
+
+              <div className='m-6 flex flex-col justify-start items-start gap-4'>
+                <p className='text-gray-400 text-sm'>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Impedit, nesciunt natus velit repellat delectus,{' '}
+                </p>
+                <button className='p-2 text-sm bg-stroke rounded-xl'>
+                  View-project
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            {/* Label Text */}
+            <p>
+              <span className='text-indigo-500 mr-1'>Project 1</span>
+              <span className='text-gray-400'>// _ui-animations</span>
+            </p>
+            {/* Actual Project Container */}
+            <div className='bg-black  rounded-2xl'>
+              <div className='upper-image'>
+                <img
+                  src='/images/image.png'
+                  alt='project-image'
+                  className='w-full object-cover'
+                />
+              </div>
+
+              <div className='m-6 flex flex-col justify-start items-start gap-4'>
+                <p className='text-gray-400 text-sm'>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Impedit, nesciunt natus velit repellat delectus,{' '}
+                </p>
+                <button className='p-2 text-sm bg-stroke rounded-xl'>
+                  View-project
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className='flex flex-col gap-2'>
+            {/* Label Text */}
+            <p>
+              <span className='text-indigo-500 mr-1'>Project 1</span>
+              <span className='text-gray-400'>// _ui-animations</span>
+            </p>
+            {/* Actual Project Container */}
+            <div className='bg-black  rounded-2xl'>
+              <div className='upper-image'>
+                <img
+                  src='/images/image.png'
+                  alt='project-image'
+                  className='w-full object-cover'
+                />
+              </div>
+
+              <div className='m-6 flex flex-col justify-start items-start gap-4'>
+                <p className='text-gray-400 text-sm'>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Impedit, nesciunt natus velit repellat delectus,{' '}
+                </p>
+                <button className='p-2 text-sm bg-stroke rounded-xl'>
+                  View-project
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className='flex flex-col gap-2'>
+            {/* Label Text */}
+            <p>
+              <span className='text-indigo-500 mr-1'>Project 1</span>
+              <span className='text-gray-400'>// _ui-animations</span>
+            </p>
+            {/* Actual Project Container */}
+            <div className=' bg-black  rounded-2xl'>
+              <div className='upper-image'>
+                <img
+                  src='/images/image.png'
+                  alt='project-image'
+                  className='w-full object-cover'
+                />
+              </div>
+              <div className='m-6 flex flex-col justify-start items-start gap-4'>
+                <p className='text-gray-400 text-sm'>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Impedit, nesciunt natus velit repellat delectus,{' '}
+                </p>
+                <button className='p-2 text-sm bg-stroke rounded-xl'>
+                  View-project
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
+
+        <GradualBlur />
       </div>
     </div>
   );
