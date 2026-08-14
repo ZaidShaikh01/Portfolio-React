a9754363af588e6d62eea1dfa683e55a5affe3e8
ZaidShaikh <szaid5146@gmail.com>
2026-08-14 15:28:02 +0530

    Working certificate section

--- app/routes/certificates/certificate-details.tsx
+++ app/routes/certificates/certificate-details.tsx
@@ -8,38 +8,28 @@ type CertificateDetailsProps = {
 const CertificateDetails = ({ section }: CertificateDetailsProps) => {
   return (
     <div className='flex-9 flex justify-center items-center flex-col gap-8 h-full w-full p-5'>
-      <div className='text-3xl text-center'>Internship Certificate Endava</div>
+      <div className='text-3xl text-center'>{section.certificateTitle}</div>
       <div className='flex  w-full justify-around'>
         <div>
           <span className='text-xl'>From - </span>
-          <span className='text-indigo-500 text-xl'>13th jan 2026</span>
+          <span className='text-indigo-500 text-xl'>{section.beginDate.toLocaleDateString()}</span>
         </div>
         <div>
           <span className='text-xl'>To - </span>
-          <span className='text-indigo-500 text-xl'>13th jan 2026</span>
+          <span className='text-indigo-500 text-xl'>{section.endDate.toLocaleDateString()}</span>
         </div>
       </div>
 
       {/* Details of certificate & Image preview*/}
       <div className='flex justify-center items-center gap-20 w-full px-30 '>
         <div className='detials w-2/4 '>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil
-          accusantium facilis explicabo est praesentium dolore. Vitae nisi
-          necessitatibus quos iure porro animi quo error beatae, accusamus, non
-          eligendi enim dolorum? Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Quae perspiciatis animi totam placeat repellendus
-          incidunt corrupti quaerat similique adipisci soluta. Aliquid
-          exercitationem consequatur temporibus fugiat eveniet assumenda,
-          tempora non placeat. Lorem ipsum dolor sit amet consectetur,
-          adipisicing elit. Magni, labore dignissimos? Sed rerum, rem a
-          molestiae quam sapiente accusamus laboriosam distinctio officiis
-          voluptatem animi nobis, esse placeat ea optio amet.
+          {section.certificateDetails}
         </div>
         <div className='image-container w-1/4'>
           <img
-            src='/images/DAVA.png'
+            src={section.certificateLogoURL}
             className=' w-full object-cover'
-            alt='endava-logo'
+            alt={section.certificateTitle}
           />
         </div>
       </div>
